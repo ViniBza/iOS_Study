fa5b3783cd46d80c9b6d0d8789298253d88600e1
ViniBza <ViniBza@gmail.com>
2024-10-29 12:13:00 +0000

    Finalizado primeiro tutorial do Scrumdinger tutorial.

---
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Vinicio Barbosa Bezerra on 15/10/2024.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MeetingView()
+}
